59836a5d768b331cd6f421da60f95f6a87a24e7f
DenisImamovicc <Danielarbete@protonmail.com>
2022-07-14 17:22:18 +0200

    Implement random qoutes when page load complete.

--- src/App.js
+++ src/App.js
@@ -17,10 +17,11 @@ function App() {
       "author": "ok"
     }
   ]
-  const [qoute, setqoute] = useState("If you always say no,you will never say yes.")
-  const [author, setauthor] = useState("Fuckboi denis")
+  const RNG=Math.floor(Math.random()*quotes.length)
+  const [qoute, setqoute] = useState(quotes[RNG].text)
+  const [author, setauthor] = useState(quotes[RNG].author)
   
-  const DisplayAnotherQuote= () =>{
+  const DisplayAnotherQuote = () =>{
     const RNG=Math.floor(Math.random()*quotes.length)
     return setqoute(quotes[RNG].text) 
         +  setauthor(quotes[RNG].author)
@@ -28,13 +29,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1 id='main-title'>Random Quoute Generator</h1>
+      <h1 id='main-title'>Random Quote Generator</h1>
       <div id="quote-box">
         <Card style={{ width: '18rem' }}>
           <Card.Body>
             <Card.Title id='text'>{qoute}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted" id='author'>From:{author}</Card.Subtitle>
-            <Button href="#" id="new-quote" onClick={DisplayAnotherQuote}>Gimme another qoute</Button>
+            <Button id="new-quote" onClick={DisplayAnotherQuote}>Gimme another qoute</Button>
             <Button href="twitter.com/intent/tweet" id="tweet-quote" target='blank'>Imma tweet it!</Button>
           </Card.Body>
         </Card>
