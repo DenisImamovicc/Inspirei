7958f79fde4c50cdb4faae90e38f039b20a6a48a
DenisImamovicc <Danielarbete@protonmail.com>
2022-07-15 12:43:07 +0200

    Fetch qoutes and display by a external api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 
 function App() {
-
+  const [qoute, setqoute] = useState("")
+  const [author, setauthor] = useState("")
+  
   const FetchQoutes = async() =>{
     const Qoute=await fetch('https://zenquotes.io/api/random')
     .then(response => response.json())
@@ -12,15 +14,6 @@ function App() {
 
     return setqoute(Qoute[0].q) + setauthor(Qoute[0].a)
   }
-  const [qoute, setqoute] = useState("")
-  const [author, setauthor] = useState("")
-  
-  // const DisplayAnotherQuote = () =>{
-  //   const RNG=Math.floor(Math.random()*quotes.length)
-  //   return setqoute(quotes[RNG].text) 
-  //       +  setauthor(quotes[RNG].author)
-  // }
-
 
   useEffect(() => {
     FetchQoutes()
@@ -33,11 +26,11 @@ function App() {
       <div id="quote-box">
         <Card className="Card"> 
           <Card.Body>
-            <Card.Title id='text' className='text-white'>{qoute}</Card.Title>
-            <Card.Subtitle className="mb-2 text-white" id='author'>- {author}</Card.Subtitle>
+            <Card.Title id='text' className='text-white fade-in'>{qoute}</Card.Title>
+            <Card.Subtitle className="mb-2 text-white fade-in" id='author'>- {author}</Card.Subtitle>
             <div className='button-group'>
-            <Button id="new-quote" className='mb-2' onClick={FetchQoutes}>Gimme another qoute</Button>
-            <Button href="twitter.com/intent/tweet" id="tweet-quote" target='blank'>Imma tweet it!</Button>
+            <Button variant="outline-dark" id="new-quote" className='mb-2 text-white' onClick={FetchQoutes}>Gimme another qoute</Button>
+            <Button variant="outline-dark" href="twitter.com/intent/tweet" id="tweet-quote" className='mb-2 text-white'  target='blank'>Imma tweet it!</Button>
             </div>
           </Card.Body>
         </Card>
