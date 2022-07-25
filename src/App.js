1c39fcaa548e80b2902b2b1717f70142f1419c5a
DenisImamovicc <Danielarbete@protonmail.com>
2022-07-25 14:35:56 +0200

    Change bg to more load freindly size,Add fade in and out when new qoutes come.

--- src/App.js
+++ src/App.js
@@ -3,61 +3,77 @@ import {useState,useEffect} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 import background from "./images/bg-desktop.jpg";
+import Fade from 'react-bootstrap/Fade';
 // import BgImgages from "./bgapicall.json"
 
 function App() { 
   const [qoute, setqoute] = useState([{}])
   const [Currqoute, setCurrqoute] = useState([{}])
   const [count, setcount] = useState(0)
+  const [open, setOpen] = useState(false);
   const API_ROUTE = `https://random-qoute-generator-api.herokuapp.com/Qoutes`
   
+  const ManageFade = ()=>{
+    setOpen((open)=>!open)
+  }
+
+
   const FetchQoutes = async () =>{
     await fetch(API_ROUTE)
     .then(response => response.json())
     .then((data) => {
+      console.log(data)
        setqoute(data)
        DisplayQoutes(data)
+
     }).catch((error)=>{
       console.error(error);
       DisplayQoutes([{q:"Server is down,but you do not have to be",a:"Denis Imamovic"}])
     });
-    //console.log(Qoute)
+    //console.log(data)
   }
 
-const DisplayQoutes = (inputData) => setCurrqoute(inputData[count])
+const DisplayQoutes = (inputData) => {
+  if(Currqoute===undefined){
+    console.log("its undefined");
+    setCurrqoute(inputData[0])
+    ManageFade()
+  }
+  setcount((count)=>count+1) 
+  setCurrqoute(inputData[count])
+  ManageFade()
+  console.log(Currqoute,count);
+}
     
    const ManageQoutes = () =>{
      if(count===qoute.length-1||count===0){
+      console.log("if passed")
+      FetchQoutes()
       setcount(0)
-      FetchQoutes()  
-      setcount((count)=>count+1) 
     }else{
-       console.log("Should not do this")
+       console.log("else passed")
        DisplayQoutes(qoute)
-       setcount((count)=>count+1) 
       }     
    }
    
    useEffect(() => {
-    FetchQoutes()
+     ManageQoutes()
     //eslint-disable-next-line react-hooks/exhaustive-deps
    },[])
+
    
-   useEffect(() => {
-    console.log("reseted count",count)
-   },[count])
    
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
-      <h1 id='main-title'>Random Quote Generator</h1>
+      <h1 id='main-title'>Inspirei</h1>
       <div id="quote-box">
         <Card className="Card"> 
           <Card.Body>
             <Card.Title id='text' className='text-white fade-in'>{Currqoute && Currqoute.q}</Card.Title>
             <Card.Subtitle className="mb-2 text-white fade-in" id='author'>- {Currqoute && Currqoute.a}</Card.Subtitle>
             <div className='button-group'>
-            <Button variant="outline-dark" id="new-quote" className='mb-2 text-white' onClick={ManageQoutes}>Gimme another qoute</Button>
-            <Button variant="outline-dark" href={`https://twitter.com/intent/tweet?text=${Currqoute && Currqoute.q} By ${Currqoute && Currqoute.a}`} id="tweet-quote" className='mb-2 text-white'  target='blank'>Imma tweet it!</Button>
+            <Button variant="primary" id="new-quote" className='mb-2 text-white' onClick={ManageQoutes}>Gimme another qoute</Button>
+            <Button variant="secondary" href={`https://twitter.com/intent/tweet?text=${Currqoute && Currqoute.q} By ${Currqoute && Currqoute.a}`} id="tweet-quote" className='mb-2 text-white'  target='blank'>Imma tweet it!</Button>
             </div>
           </Card.Body>
         </Card>
@@ -65,5 +81,8 @@ const DisplayQoutes = (inputData) => setCurrqoute(inputData[count])
     </div>
   );
 }
+
+
+
 
 export default App;
